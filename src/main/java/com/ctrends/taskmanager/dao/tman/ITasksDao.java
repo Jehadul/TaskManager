6f606b4d5c80dbcb234d8b04f08098c305c5e8d8
irin <irin@gmail.com>
2017-02-28 17:29:47 +0600

    create ui for task create

--- src/main/java/com/ctrends/taskmanager/dao/tman/ITasksDao.java
+++ src/main/java/com/ctrends/taskmanager/dao/tman/ITasksDao.java
@@ -1,8 +1,14 @@
 package com.ctrends.taskmanager.dao.tman;
 
+import java.util.List;
+
 import com.ctrends.taskmanager.dao.ICommonDao;
+import com.ctrends.taskmanager.model.taskmanage.Module;
+import com.ctrends.taskmanager.model.taskmanage.Suite;
 import com.ctrends.taskmanager.model.tman.Tasks;
 
 public interface ITasksDao extends ICommonDao<Tasks> {
-
+	List<Suite> getAllSuites();
+	List<Module> getAllModules();
+	List<Module> getBySuit(String suitCode);
 }
