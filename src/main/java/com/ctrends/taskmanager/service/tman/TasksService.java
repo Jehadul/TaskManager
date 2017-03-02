1d8449d5ac208328e26e3f881503e91601ef0289
Your Name <kamrun@gmail.com>
2017-03-02 09:33:21 +0600

    tasks service modify

--- src/main/java/com/ctrends/taskmanager/service/tman/TasksService.java
+++ src/main/java/com/ctrends/taskmanager/service/tman/TasksService.java
@@ -1,20 +1,42 @@
 package com.ctrends.taskmanager.service.tman;
 
+import java.sql.Timestamp;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.ctrends.taskmanager.dao.tman.ITasksDao;
+import com.ctrends.taskmanager.model.taskmanage.Module;
+import com.ctrends.taskmanager.model.taskmanage.Suite;
 import com.ctrends.taskmanager.model.tman.Tasks;
 
 @Repository("tasksService")
 public class TasksService implements ITasksService {
 
+	@Autowired
+	ITasksDao tasksDao;
+	
 	@Override
 	public Map<String, String> insert(Map<String, String[]> requestMap) {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, String> data = new HashMap<String,String>();
+		
+		/*Suite suite	=new Suite();
+		suite.setSuiteCode(requestMap.get("suite_code")[0]);*/
+		Tasks tasks=new Tasks();
+		tasks.setSuiteCode(requestMap.get("suite_code")[0]);
+		tasks.setDescription(requestMap.get("description")[0]);
+		tasks.setStoryCode(requestMap.get("story_code")[0]);
+	    tasks.setModuleCode(requestMap.get("module_code")[0]);
+		tasks.setTaskTitle(requestMap.get("task_title")[0]);
+		tasks.setEstimatedTime(requestMap.get("estimated_time")[0]);
+		tasks.setAsignee(requestMap.get("assignee")[0]);
+		UUID id = tasksDao.insertDoc(tasks);
+		data.put("id", id.toString());
+		return data ;
 	}
 
 	@Override
@@ -25,8 +47,7 @@ public class TasksService implements ITasksService {
 
 	@Override
 	public Tasks getById(UUID id) {
-		// TODO Auto-generated method stub
-		return null;
+		return tasksDao.getDocById(id);
 	}
 
 	@Override
