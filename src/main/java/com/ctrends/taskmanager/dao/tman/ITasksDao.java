29c89e074f8e3829b443dcc57ae451301f3fc092
Your Name <kamrun@gmail.com>
2017-03-02 17:28:23 +0600

    modified

--- src/main/java/com/ctrends/taskmanager/dao/tman/ITasksDao.java
+++ src/main/java/com/ctrends/taskmanager/dao/tman/ITasksDao.java
@@ -5,10 +5,12 @@ import java.util.List;
 import com.ctrends.taskmanager.dao.ICommonDao;
 import com.ctrends.taskmanager.model.taskmanage.Module;
 import com.ctrends.taskmanager.model.taskmanage.Suite;
+import com.ctrends.taskmanager.model.taskmanage.product.Product;
 import com.ctrends.taskmanager.model.tman.Tasks;
 
 public interface ITasksDao extends ICommonDao<Tasks> {
 	List<Suite> getAllSuites();
+	List<Product> getAllProducts();
 	List<Module> getAllModules();
 	List<Module> getBySuit(String suitCode);
 }
