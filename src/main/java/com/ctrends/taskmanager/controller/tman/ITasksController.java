c08b514b5fc7f5291c1b9415a5caa5dffe6c37b0
Zihad <zihad@gmail.com>
2017-03-04 17:12:59 +0600

    add

--- src/main/java/com/ctrends/taskmanager/controller/tman/ITasksController.java
+++ src/main/java/com/ctrends/taskmanager/controller/tman/ITasksController.java
@@ -1,13 +1,10 @@
 package com.ctrends.taskmanager.controller.tman;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.web.servlet.ModelAndView;
 
 import com.ctrends.taskmanager.controller.ICommonController;
 import com.ctrends.taskmanager.model.tman.Tasks;
 
 public interface ITasksController extends ICommonController<Tasks> {
 
-	public ModelAndView create(HttpServletRequest request);
+	
 }
