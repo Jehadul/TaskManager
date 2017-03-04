f29bcd9f07da928b7a4cdc640cabf82e0509e712
irin <irin@gmail.com>
2017-03-04 10:06:31 +0600

    new add

---
+++ src/test/java/com/ctrends/taskmanager/service/TaskServiceTest.java
@@ -0,0 +1,14 @@
+package com.ctrends.taskmanager.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaskServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
