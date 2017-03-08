d947ad58ecc9a24f31e98ba75a64c5fdd90abb27
Your Name <kamrun@gmail.com>
2017-03-08 14:17:36 +0600

    modify task controller test dao test service test

--- src/test/java/com/ctrends/taskmanager/dao/TaskDaoTest.java
+++ src/test/java/com/ctrends/taskmanager/dao/TaskDaoTest.java
@@ -2,13 +2,71 @@ package com.ctrends.taskmanager.dao;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.List;
+import java.util.UUID;
 
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ctrends.taskmanager.dao.tman.ITasksDao;
+import com.ctrends.taskmanager.dao.tman.TasksDao;
+import com.ctrends.taskmanager.model.taskmanage.Module;
+import com.ctrends.taskmanager.model.taskmanage.Suite;
+import com.ctrends.taskmanager.model.taskmanage.product.Product;
+import com.ctrends.taskmanager.model.tman.Tasks;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/spring-database.xml", 
+									"/spring-dispatcher-servlet.xml", 
+									"/spring-security.xml" })
 public class TaskDaoTest {
 
+	@Autowired
+	ITasksDao taskDao;
+	
+	@Autowired
+	private SessionFactory sessionfactory;
+	
+	public UUID id;
+	String suiteCode="FIN4";
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetAllDoc() {
+		
+		List<Tasks> tasksLi=taskDao.getAllDoc();
+		assertNotNull(tasksLi);
 	}
-
+	
+	
+	@Test
+	public void testGetAllSuites() {
+		
+		List<Suite> suiteLi=taskDao.getAllSuites();
+		assertNotNull(suiteLi);
+		
+	}
+	
+	
+	@Test
+	public void testGetAllModules() {
+	
+		List<Module> moduleLi = taskDao.getAllModules();
+		assertNotNull(moduleLi);
+	}
+	
+	
+	@Test
+	public void testGetBySuit() {
+		
+		List<Module> modList = taskDao.getBySuit(suiteCode);
+		assertNotNull(modList);
+	}
 }
