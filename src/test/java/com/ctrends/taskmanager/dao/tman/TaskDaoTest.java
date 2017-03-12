3c0fc231e298ee2766e0549237296e7109d2199e
irin <irin@gmail.com>
2017-03-12 14:27:31 +0600

    modify task dao test class

--- src/test/java/com/ctrends/taskmanager/dao/tman/TaskDaoTest.java
+++ src/test/java/com/ctrends/taskmanager/dao/tman/TaskDaoTest.java
@@ -1,19 +1,29 @@
 package com.ctrends.taskmanager.dao.tman;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ctrends.taskmanager.dao.tman.ITasksDao;
+import static org.mockito.Mockito.when;
+import com.ctrends.taskmanager.dao.tman.TasksDao;
 import com.ctrends.taskmanager.model.taskmanage.Module;
+import com.ctrends.taskmanager.model.taskmanage.PrivGroup;
 import com.ctrends.taskmanager.model.taskmanage.Suite;
 import com.ctrends.taskmanager.model.tman.Tasks;
 
@@ -30,15 +40,26 @@ public class TaskDaoTest {
 	@Autowired
 	private SessionFactory sessionfactory;
 	
-	public UUID id;
-	String suiteCode="FIN4";
+	String suiteCode=null;
+	String moduleCode=null;
+	
+	//@Mock
+	private UUID id=UUID.fromString("10462413-c4ad-4608-93c7-08476b6d2b92");
+
+	@Mock
+	private List<Tasks> mockTtasksList;
 	
 	
 	@Test
-	public void testGetAllDoc() {
+	public void testGetAllDoc(){
 		
 		List<Tasks> tasksLi=taskDao.getAllDoc();
 		assertNotNull(tasksLi);
+//		//List<Tasks> tasksLi=taskDao.getAllDoc();
+//		mockTtasksList =new ArrayList<>();
+//		Tasks mockTasks=Mockito.mock(Tasks.class);
+//		mockTtasksList.add(mockTasks);
+//		when(taskDao.getAllDoc()).thenReturn(mockTtasksList);
 	}
 	
 	
@@ -52,7 +73,7 @@ public class TaskDaoTest {
 	
 	
 	@Test
-	public void testGetAllModules() { 
+	public void testGetAllModules() {
 	
 		List<Module> moduleLi = taskDao.getAllModules();
 		assertNotNull(moduleLi);
@@ -64,5 +85,56 @@ public class TaskDaoTest {
 		
 		List<Module> modList = taskDao.getBySuit(suiteCode);
 		assertNotNull(modList);
+	}
+	
+
+	@Test
+	public void testGetByModule() {
+		
+		List<PrivGroup> privGrpList = taskDao.getPrivGroup(suiteCode, moduleCode);
+		assertNotNull(privGrpList);
+	}
+	
+	
+	@Test
+	public void testGetDocById() {
+		Tasks task = taskDao.getDocById(id);
+		assertEquals(new Tasks().getClass(), task.getClass());
+	}
+	
+	@Test
+	public void testGetDocs(){
+		
+		Map<String, String> params=new LinkedHashMap<>();
+		List<Tasks> tasksLi=taskDao.getDocs(params);
+		assertNotNull(tasksLi);
+		
+	}
+	
+	@Test
+	public void testInsertDoc(){
+		Tasks doc=new Tasks();
+		UUID inId = taskDao.insertDoc(doc); 
+		assertEquals(id.getClass(), inId.getClass());
+	}
+	
+	@Test
+	public void testUpdateDoc(){
+		Tasks doc=new Tasks();
+		UUID inId = taskDao.updateDoc(doc); 
+		assertEquals(id.getClass(), inId.getClass());
+	}
+	
+	@Test
+	public void testDeleteDoc(){
+		UUID inId = taskDao.deleteDoc(id); 
+		assertEquals(id.getClass(), inId.getClass());
+	}
+	
+	@Test
+	public void testGetAllPrivGrps(){
+		List<PrivGroup> privGrpList = taskDao.getAllPrivGrps();
+		assertNotNull(privGrpList);
+		
 	}
 }
