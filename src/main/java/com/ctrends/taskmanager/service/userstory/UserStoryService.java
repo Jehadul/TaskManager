2894258f139b7a1692099592454cd0cef1e11080
Your Name <kamrun@gmail.com>
2017-03-14 12:53:46 +0600

    modify method edit related

--- src/main/java/com/ctrends/taskmanager/service/userstory/UserStoryService.java
+++ src/main/java/com/ctrends/taskmanager/service/userstory/UserStoryService.java
@@ -1,13 +1,30 @@
 package com.ctrends.taskmanager.service.userstory;
 
+import java.sql.Timestamp;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import com.ctrends.taskmanager.model.userstory.UserStory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.ctrends.taskmanager.dao.tman.ITasksDao;
+import com.ctrends.taskmanager.dao.userstory.IUserStoryDAO;
+import com.ctrends.taskmanager.model.tman.Tasks;
+import com.ctrends.taskmanager.model.user.User;
+import com.ctrends.taskmanager.model.userstory.UserStory;
+import com.ctrends.taskmanager.service.user.IUserService;
+
+@Service("userStoryService")
 public class UserStoryService implements IUserStoryService {
 
+	@Autowired
+	IUserStoryDAO userStoryDAO;
+	
+	@Autowired
+	IUserService userService;
+	
 	@Override
 	public Map<String, String> insert(Map<String, String[]> requestMap) {
 		// TODO Auto-generated method stub
@@ -22,14 +39,72 @@ public class UserStoryService implements IUserStoryService {
 
 	@Override
 	public UserStory getById(UUID id) {
-		// TODO Auto-generated method stub
-		return null;
+		return userStoryDAO.getDocById(id);
+		
 	}
 
 	@Override
 	public Map<String, String> update(Map<String, String[]> requestMap) {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, String> data = new HashMap<String, String>();
+		User currentUser = userService.getCurrentUser();
+		//System.out.println(":::::"+requestMap.get("id")[0]);
+		UserStory userStory = userStoryDAO.getDocById(UUID.fromString(requestMap.get("id")[0]));
+		
+		userStory.setSuiteCode(requestMap.get("suite_code")[0]);
+		userStory.setSuiteName(requestMap.get("suite_name")[0]);
+		userStory.setModuleCode(requestMap.get("module_code")[0]);
+		userStory.setModuleName(requestMap.get("module_name")[0]);
+		userStory.setPrivGrpCode(Integer.parseInt(requestMap.get("priv_grp_code")[0]));
+		userStory.setPrivGrpName(requestMap.get("priv_grp_name")[0]);	
+		userStory.setDescription(requestMap.get("description")[0]);
+		//userStory.setStoryCode(requestMap.get("story_code")[0]);
+	    
+		//userStory.setTaskTitle(requestMap.get("task_title")[0]);
+		//userStory.setEstimatedTime(Double.parseDouble(requestMap.get("estimated_time")[0]));
+		//userStory.setAsignee(requestMap.get("assignee")[0]);	
+		
+		
+		userStory.setClientCode(currentUser.getClientCode());
+		userStory.setClientName(currentUser.getClientName());
+		userStory.setCompanyCode(currentUser.getCompanyCode());
+		userStory.setCompanyName(currentUser.getCompanyName());
+		userStory.setUpdatedByCode(currentUser.getEmpCode());            
+		userStory.setUpdatedByName(currentUser.getEmpName());
+		userStory.setUpdatedByUsername(currentUser.getUsername());
+		userStory.setUpdatedByEmail(currentUser.getEmail());
+		userStory.setUpdatedByCompanyCode(currentUser.getCompanyCode());
+		userStory.setUpdatedByCompanyName(currentUser.getCompanyName());
+		userStory.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
+		
+		userStory.setUpdatedByCode(currentUser.getUpdatedByCode());
+		userStory.setUpdatedByName(currentUser.getUpdatedByName());
+		userStory.setUpdatedByCompanyCode(currentUser.getCompanyCode());
+		userStory.setUpdatedByCompanyName(currentUser.getCompanyName());
+		userStory.setUpdatedByEmail(currentUser.getEmail());
+		userStory.setUpdatedByUsername(currentUser.getUsername());
+		
+		userStory.setClientCode(currentUser.getClientCode());
+		userStory.setClientName(currentUser.getClientName());
+		userStory.setCompanyCode(currentUser.getCompanyCode());
+		userStory.setCompanyName(currentUser.getCompanyName());
+		userStory.setUpdatedByCode(currentUser.getEmpCode());            
+		userStory.setUpdatedByName(currentUser.getEmpName());
+		userStory.setUpdatedByUsername(currentUser.getUsername());
+		userStory.setUpdatedByEmail(currentUser.getEmail());
+		userStory.setUpdatedByCompanyCode(currentUser.getCompanyCode());
+		userStory.setUpdatedByCompanyName(currentUser.getCompanyName());
+		userStory.setUpdatedAt(new Timestamp(System.currentTimeMillis()));
+		
+		userStory.setUpdatedByCode(currentUser.getUpdatedByCode());
+		userStory.setUpdatedByName(currentUser.getUpdatedByName());
+		userStory.setUpdatedByCompanyCode(currentUser.getCompanyCode());
+		userStory.setUpdatedByCompanyName(currentUser.getCompanyName());
+		userStory.setUpdatedByEmail(currentUser.getEmail());
+		userStory.setUpdatedByUsername(currentUser.getUsername());
+		
+		UUID id = userStoryDAO.updateDoc(userStory);
+		data.put("id", id.toString());
+		return data;
 	}
 
 	@Override
