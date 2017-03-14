97f03e2566ec45bad7f8a18c812eb9fbd339f839
irin <irin@gmail.com>
2017-03-14 11:18:47 +0600

    modify user story

--- src/main/java/com/ctrends/taskmanager/dao/userstory/IUserStoryDAO.java
+++ src/main/java/com/ctrends/taskmanager/dao/userstory/IUserStoryDAO.java
@@ -1,8 +1,23 @@
 package com.ctrends.taskmanager.dao.userstory;
 
+import java.util.List;
+
 import com.ctrends.taskmanager.dao.ICommonDAO;
+import com.ctrends.taskmanager.model.taskmanage.Module;
+import com.ctrends.taskmanager.model.taskmanage.PrivGroup;
+import com.ctrends.taskmanager.model.taskmanage.Suite;
 import com.ctrends.taskmanager.model.userstory.UserStory;
 
 public interface IUserStoryDAO extends ICommonDAO<UserStory> {
+
+	List<Module> getBySuit(String suitCode);
+
+	List<Suite> getAllSuites();
+
+	List<Module> getAllModules();
+
+	List<PrivGroup> getAllPrivGrps();
+
+	List<PrivGroup> getPrivGroup(String suiteCode, String modeCode);
 
 }
