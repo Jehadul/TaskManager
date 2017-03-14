af4f4abbe5d7eeb51118d445a9735f142ed9640b
irin <irin@gmail.com>
2017-03-14 09:24:08 +0600

    add user story dao package

---
+++ src/main/java/com/ctrends/taskmanager/dao/userstory/IUserStoryDAO.java
@@ -0,0 +1,8 @@
+package com.ctrends.taskmanager.dao.userstory;
+
+import com.ctrends.taskmanager.dao.ICommonDAO;
+import com.ctrends.taskmanager.model.userstory.UserStory;
+
+public interface IUserStoryDAO extends ICommonDAO<UserStory> {
+
+}
---
+++ src/main/java/com/ctrends/taskmanager/dao/userstory/UserStoryDAO.java
@@ -0,0 +1,47 @@
+package com.ctrends.taskmanager.dao.userstory;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import com.ctrends.taskmanager.model.userstory.UserStory;
+
+public class UserStoryDAO implements IUserStoryDAO {
+
+	@Override
+	public List<UserStory> getAllDoc() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserStory getDocById(UUID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserStory> getDocs(Map<String, String> params) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UUID insertDoc(UserStory doc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UUID updateDoc(UserStory doc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UUID deleteDoc(UUID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
