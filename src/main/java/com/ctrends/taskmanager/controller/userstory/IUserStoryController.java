a38148b9a531dfdb5bc831846636368acf24a961
irin <irin@gmail.com>
2017-03-14 11:52:02 +0600

    modify story

--- src/main/java/com/ctrends/taskmanager/controller/userstory/IUserStoryController.java
+++ src/main/java/com/ctrends/taskmanager/controller/userstory/IUserStoryController.java
@@ -1,8 +1,14 @@
 package com.ctrends.taskmanager.controller.userstory;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.servlet.ModelAndView;
+
 import com.ctrends.taskmanager.controller.ICommonController;
 import com.ctrends.taskmanager.model.userstory.UserStory;
 
 public interface IUserStoryController extends ICommonController<UserStory> {
+
+	ModelAndView create(HttpServletRequest request);
 
 }
