d2b0c1c07456bb83edfd08b154c3745b07e26b32
irin <irin@gmail.com>
2017-03-16 14:57:41 +0600

    check test class modifying id value

--- src/test/java/com/ctrends/taskmanager/dao/tman/TaskDaoTest.java
+++ src/test/java/com/ctrends/taskmanager/dao/tman/TaskDaoTest.java
@@ -46,7 +46,7 @@ public class TaskDaoTest {
 	String moduleCode=null;
 	
 	//@Mock
-	private UUID id=UUID.fromString("cfc6ed41-bf75-41f8-8b65-93250aaec387");
+	private UUID id=UUID.fromString("fe443488-131c-42d5-8131-da2a2e971774");
 
 	@Mock
 	private List<Tasks> mockTtasksList;
@@ -65,7 +65,8 @@ public class TaskDaoTest {
 	
 	@Test
 	public void testGetDocById_ReturnTasks() {
-		Tasks task = taskDao.getDocById(id);
+		 UUID uid=UUID.fromString("1d889f43-aa74-42b9-a337-5702770ecd13");
+		Tasks task = taskDao.getDocById(uid);
 		assertEquals(new Tasks().getClass(), task.getClass());
 	}
 	
