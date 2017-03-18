b3a63e884c373959ad7eef36f5c78a198b593f65
irin <irin@gmail.com>
2017-03-18 09:24:25 +0600

    add test case

--- src/test/java/com/ctrends/taskmanager/controller/common/HomeControllerTest.java
+++ src/test/java/com/ctrends/taskmanager/controller/common/HomeControllerTest.java
@@ -1,17 +1,20 @@
 package com.ctrends.taskmanager.controller.common;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.ctrends.taskmanager.bean.WSResponse;
+import com.google.gson.Gson;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/spring-database.xml", 
@@ -37,30 +40,50 @@ public class HomeControllerTest {
 		ModelAndView mav = homeController.index();
 		assertTrue(mav.hasView());
 	}
+	
+	@Test
+	public void testDashboard_ReturnsModelAndView(){
+		ModelAndView mav = homeController.dashboard();
+		assertTrue(mav.hasView());
+	}
+	
+	@Test
+	@WithMockUser("CTS0104")
+	public void testNoticeboard_ReturnsModelAndView(){
+		ModelAndView mav = homeController.noticeboard();
+		assertTrue(mav.hasView());
+	}
+
 
 	@Test
-	public void testLogin() {
-		fail("Not yet implemented");
+	public void testLogin_ReturnsModelAndView() {
+		String a = "adv";
+		ModelAndView mav = homeController.login(a, a);
+		assertTrue(mav.hasView());
 	}
 
 	@Test
-	public void testLogout() {
-		fail("Not yet implemented");
+	public void testLogout_ReturnsModelAndView() {
+		ModelAndView mav = homeController.logout();
+		assertTrue(mav.hasView());
 	}
 
 	@Test
-	public void testAccesssDenied() {
-		fail("Not yet implemented");
+	public void testAccesssDenied_ReturnsModelAndView() {
+		ModelAndView mav = homeController.accesssDenied();
+		assertTrue(mav.hasView());
 	}
 
 	@Test
-	public void testLoadpref() {
-		fail("Not yet implemented");
+	public void testLoadpref_ReturnsString() {
+		String g = homeController.loadpref(request);
+		assertEquals(String.class, g.getClass());
 	}
 
 	@Test
-	public void testTcodesearch() {
-		fail("Not yet implemented");
+	public void testTcodesearch_ReturnsWSResponse() {
+		WSResponse ar = homeController.tcodesearch(request);
+		assertEquals(WSResponse.class, ar.getClass());
 	}
 
 }
