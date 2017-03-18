2adbad2cd53208f68920ffdae3dbb3794f6045b7
Iqbal Hossain <iqbal.hossain@ctrends-software.com>
2017-03-18 16:07:04 +0600

    modify

--- src/main/java/com/ctrends/taskmanager/service/user/IUserService.java
+++ src/main/java/com/ctrends/taskmanager/service/user/IUserService.java
@@ -1,5 +1,8 @@
 package com.ctrends.taskmanager.service.user;
 
+import java.util.List;
+import java.util.Map;
+
 import com.ctrends.taskmanager.model.user.User;
 import com.ctrends.taskmanager.service.ICommonService;
 
@@ -7,5 +10,6 @@ public interface IUserService extends ICommonService<User> {
 
 	User getUserByUserName(String username);
 	User getCurrentUser();
+	List<User> find(Map<String, String> searchingKey);
 
 }
