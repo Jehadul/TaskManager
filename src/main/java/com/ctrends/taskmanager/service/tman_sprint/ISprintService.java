bfd293987d91e974195bf27ed8ed8b92111fc650
Farid Ahmed <w3farid@gmail.com>
2017-03-23 20:01:38 +0600

    sprint midified

--- src/main/java/com/ctrends/taskmanager/service/tman_sprint/ISprintService.java
+++ src/main/java/com/ctrends/taskmanager/service/tman_sprint/ISprintService.java
@@ -1,8 +1,13 @@
 package com.ctrends.taskmanager.service.tman_sprint;
 
+import java.util.List;
+
 import com.ctrends.taskmanager.model.tman_sprint.SprintManager;
+import com.ctrends.taskmanager.model.tman_sprint.SprintManagerDetails;
 import com.ctrends.taskmanager.service.ICommonService;
 
 public interface ISprintService extends ICommonService<SprintManager> {
+
+	public List<SprintManagerDetails> getByIdSprintCode(String sprintCode);
 
 }
