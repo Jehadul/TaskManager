84dda72c265275ccf8982f63097cf15c663d1f6a
irin <irin@gmail.com>
2017-03-28 12:35:44 +0600

    modify sprint page

--- src/main/java/com/ctrends/taskmanager/service/tman_sprint/ISprintService.java
+++ src/main/java/com/ctrends/taskmanager/service/tman_sprint/ISprintService.java
@@ -1,6 +1,7 @@
 package com.ctrends.taskmanager.service.tman_sprint;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.ctrends.taskmanager.model.tman_sprint.SprintManager;
 import com.ctrends.taskmanager.model.tman_sprint.SprintManagerDetails;
@@ -9,5 +10,7 @@ import com.ctrends.taskmanager.service.ICommonService;
 public interface ISprintService extends ICommonService<SprintManager> {
 
 	public List<SprintManagerDetails> getByIdSprintCode(String sprintCode);
+
+	List<SprintManagerDetails> getBySprintId(UUID id);
 
 }
