eeff1f57bed853db6b3563be55163037cc9beae3
aeontanvir <aeontanvir@gmail.com>
2017-04-03 13:09:12 +0600

    modify team insert and shwo

--- src/main/java/com/ctrends/taskmanager/controller/team/TeamController.java
+++ src/main/java/com/ctrends/taskmanager/controller/team/TeamController.java
@@ -1,9 +1,11 @@
 package com.ctrends.taskmanager.controller.team;
 
+import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,11 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ctrends.taskmanager.bean.WSResponse;
+import com.ctrends.taskmanager.dao.team.ITeamDAO;
+import com.ctrends.taskmanager.service.team.ITeamService;
 
 @RestController
 @RequestMapping("/taskman/team")
 public class TeamController implements ITeamController {
 
+	@Autowired
+	ITeamService teamService;
+	
+	
+	
+	
+	
 	@Override
 	public ModelAndView index() {
 		// TODO Auto-generated method stub
@@ -40,10 +51,15 @@ public class TeamController implements ITeamController {
 		return null;
 	}
 
+	@RequestMapping(value = "/store", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	@Override
 	public WSResponse store(HttpServletRequest request) {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, String[]> team = request.getParameterMap();
+
+		Map<String, String> data = teamService.insert(team);
+
+		return new WSResponse("success", "Saved successfully", UUID.fromString(data.get("id")), null, data.get("mode"),
+				data);
 	}
 
 	@Override
