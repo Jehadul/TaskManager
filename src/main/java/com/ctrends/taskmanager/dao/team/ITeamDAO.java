61640b1a6a6f009a70caa29e89f49d3cfadf75d1
irin <irin@gmail.com>
2017-04-03 10:37:29 +0600

    add team package in service controller dao and model

---
+++ src/main/java/com/ctrends/taskmanager/dao/team/ITeamDAO.java
@@ -0,0 +1,8 @@
+package com.ctrends.taskmanager.dao.team;
+
+import com.ctrends.taskmanager.dao.ICommonDAO;
+import com.ctrends.taskmanager.model.team.Team;
+
+public interface ITeamDAO extends ICommonDAO<Team> {
+
+}
