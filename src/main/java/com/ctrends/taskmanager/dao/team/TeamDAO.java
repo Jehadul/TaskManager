ff6ab63e8397ef851f831a0421e0b9021368b1e3
irin <irin@gmail.com>
2017-04-03 11:14:28 +0600

    add

--- src/main/java/com/ctrends/taskmanager/dao/team/TeamDAO.java
+++ src/main/java/com/ctrends/taskmanager/dao/team/TeamDAO.java
@@ -4,14 +4,38 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import com.ctrends.taskmanager.model.team.Team;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ctrends.taskmanager.dao.user.IUserDAO;
+import com.ctrends.taskmanager.model.team.Team;
+import com.ctrends.taskmanager.model.tman.Tasks;
+import com.ctrends.taskmanager.model.user.User;
+import com.ctrends.taskmanager.service.user.IUserService;
+
+@Repository("teamDao")
 public class TeamDAO implements ITeamDAO {
 
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Autowired
+	IUserDAO userDAO;
+
+	@Autowired
+	IUserService userService;
+	
+	@Transactional
 	@Override
 	public List<Team> getAllDoc() {
-		// TODO Auto-generated method stub
-		return null;
+		User currentUser = userService.getCurrentUser();
+		Query query = sessionFactory.getCurrentSession().createQuery("From Team where createdByUsername =:userName");
+		query.setParameter("userName", currentUser.getUsername());
+		List<Team> teamLi = query.list();
+		return teamLi;
 	}
 
 	@Override
