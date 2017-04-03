f8cedd3cab59a4c197c4103c48b07d5c76fe99a6
Zihad <zihad@gmail.com>
2017-04-03 11:00:14 +0600

    modify team

--- src/main/java/com/ctrends/taskmanager/controller/team/ITeamController.java
+++ src/main/java/com/ctrends/taskmanager/controller/team/ITeamController.java
@@ -1,8 +1,14 @@
 package com.ctrends.taskmanager.controller.team;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.servlet.ModelAndView;
+
 import com.ctrends.taskmanager.controller.ICommonController;
 import com.ctrends.taskmanager.model.team.Team;
 
 public interface ITeamController extends ICommonController<Team> {
+
+	ModelAndView create(HttpServletRequest request);
 
 }
