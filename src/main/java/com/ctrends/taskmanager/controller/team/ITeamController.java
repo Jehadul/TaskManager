d2be788f2a460a5298c0d899e087ecab450f292b
irin <irin@gmail.com>
2017-04-03 10:37:29 +0600

    add team package in service controller dao and model

---
+++ src/main/java/com/ctrends/taskmanager/controller/team/ITeamController.java
@@ -0,0 +1,8 @@
+package com.ctrends.taskmanager.controller.team;
+
+import com.ctrends.taskmanager.controller.ICommonController;
+import com.ctrends.taskmanager.model.team.Team;
+
+public interface ITeamController extends ICommonController<Team> {
+
+}
---
+++ src/main/java/com/ctrends/taskmanager/controller/team/TeamController.java
@@ -0,0 +1,73 @@
+package com.ctrends.taskmanager.controller.team;
+
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ctrends.taskmanager.bean.WSResponse;
+
+public class TeamController implements ITeamController {
+
+	@Override
+	public ModelAndView index() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView show(UUID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WSResponse get(UUID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WSResponse store(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView edit(UUID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WSResponse update(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WSResponse destroy(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView showSearch(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String search(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
