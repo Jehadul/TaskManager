f90bf4f5ab78d206638001b5e60b023b8dc6d89f
irin <irin@gmail.com>
2017-04-03 17:24:09 +0600

    modify team

--- src/main/java/com/ctrends/taskmanager/service/team/ITeamService.java
+++ src/main/java/com/ctrends/taskmanager/service/team/ITeamService.java
@@ -1,11 +1,15 @@
 package com.ctrends.taskmanager.service.team;
 
+import java.util.List;
 import java.util.Map;
 
 import com.ctrends.taskmanager.model.team.Team;
+import com.ctrends.taskmanager.model.team.TeamMemberDetails;
 import com.ctrends.taskmanager.service.ICommonService;
 
 public interface ITeamService extends ICommonService<Team> {
+
+	List<TeamMemberDetails> getByTeamCode(String teamCode);
 
 
 }
