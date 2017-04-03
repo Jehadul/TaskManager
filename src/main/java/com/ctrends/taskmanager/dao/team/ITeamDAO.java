e47da1202d7bbca27657c181a2198f12fe25ee40
irin <irin@gmail.com>
2017-04-03 17:24:09 +0600

    modify team

--- src/main/java/com/ctrends/taskmanager/dao/team/ITeamDAO.java
+++ src/main/java/com/ctrends/taskmanager/dao/team/ITeamDAO.java
@@ -1,12 +1,16 @@
 package com.ctrends.taskmanager.dao.team;
 
+import java.util.List;
 import java.util.Map;
 
 import com.ctrends.taskmanager.dao.ICommonDAO;
 import com.ctrends.taskmanager.model.team.Team;
+import com.ctrends.taskmanager.model.team.TeamMemberDetails;
 
 public interface ITeamDAO extends ICommonDAO<Team> {
 
 	boolean checkUnique(Map<String, Object> param);
+
+	List<TeamMemberDetails> getDocByIdTeamCode(String teamCode);
 
 }
