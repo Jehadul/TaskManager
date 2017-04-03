ce4c99c1359456f108ec5fb4be1d34c5c6ba11c8
aeontanvir <aeontanvir@gmail.com>
2017-04-03 17:20:18 +0600

    add team show page

--- src/main/java/com/ctrends/taskmanager/dao/team/ITeamDAO.java
+++ src/main/java/com/ctrends/taskmanager/dao/team/ITeamDAO.java
@@ -1,12 +1,16 @@
 package com.ctrends.taskmanager.dao.team;
 
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import com.ctrends.taskmanager.dao.ICommonDAO;
 import com.ctrends.taskmanager.model.team.Team;
+import com.ctrends.taskmanager.model.team.TeamMemberDetails;
 
 public interface ITeamDAO extends ICommonDAO<Team> {
 
 	boolean checkUnique(Map<String, Object> param);
+	public List<TeamMemberDetails> getTeamMemberDetailsByTeamId(UUID teamId);
 
 }
