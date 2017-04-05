94c37fd2891c928ce0b715f0dbf6a8a07bda1fb4
Your Name <kamrun@gmail.com>
2017-04-05 14:54:23 +0600

    modified

--- src/main/java/com/ctrends/taskmanager/controller/task_status/ITaskStatusController.java
+++ src/main/java/com/ctrends/taskmanager/controller/task_status/ITaskStatusController.java
@@ -4,11 +4,13 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ctrends.taskmanager.bean.WSResponse;
 import com.ctrends.taskmanager.controller.ICommonController;
 import com.ctrends.taskmanager.model.task_status.TaskDetails;
 
 public interface ITaskStatusController extends ICommonController<TaskDetails> {
 	public String loaddetask(HttpServletRequest request);
 	ModelAndView create(HttpServletRequest request);
+	WSResponse manageselection(HttpServletRequest request);
 	
 }
