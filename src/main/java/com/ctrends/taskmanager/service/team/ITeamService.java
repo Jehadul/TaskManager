237de5c37ec3056afffdf557227f1d1c936fc718
irin <irin@gmail.com>
2017-04-08 09:58:03 +0600

    modified

--- src/main/java/com/ctrends/taskmanager/service/team/ITeamService.java
+++ src/main/java/com/ctrends/taskmanager/service/team/ITeamService.java
@@ -5,13 +5,13 @@ import java.util.Map;
 import java.util.UUID;
 
 import com.ctrends.taskmanager.model.team.Team;
-import com.ctrends.taskmanager.model.team.TeamMemberDetails;
+import com.ctrends.taskmanager.model.team.TeamDetails;
 import com.ctrends.taskmanager.service.ICommonService;
 
 public interface ITeamService extends ICommonService<Team> {
 
-	List<TeamMemberDetails> getByTeamCode(String teamCode);
-	public List<TeamMemberDetails> getTeamMemberDetailsByTeamId(UUID teamId);
+	List<TeamDetails> getByTeamCode(String teamCode);
+	public List<TeamDetails> getTeamMemberDetailsByTeamId(UUID teamId);
 	List<Team> find(Map<String, String> searchingKey);
 
 }
