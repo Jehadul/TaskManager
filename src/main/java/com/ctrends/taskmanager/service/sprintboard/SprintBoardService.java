1c677f3392cfee3ca58f796f96a0de93846b023c
aeontanvir <aeontanvir@gmail.com>
2017-04-09 17:56:02 +0600

    sprintboard dao service controller add

---
+++ src/main/java/com/ctrends/taskmanager/service/sprintboard/SprintBoardService.java
@@ -0,0 +1,46 @@
+package com.ctrends.taskmanager.service.sprintboard;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ctrends.taskmanager.model.tman_sprint.SprintManager;
+
+public class SprintBoardService implements ISprintBoardService {
+
+	
+	
+	
+	@Override
+	public Map<String, String> insert(Map<String, String[]> requestMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<SprintManager> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SprintManager getById(UUID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, String> update(Map<String, String[]> requestMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UUID delete(Map<String, String[]> requestMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
