190e0602efa229d256b892f6cd538eb31ceb6499
aeontanvir <aeontanvir@gmail.com>
2017-04-09 18:07:48 +0600

    modify sprintboard service and controller

--- src/main/java/com/ctrends/taskmanager/controller/sprintboard/SprintBoardController.java
+++ src/main/java/com/ctrends/taskmanager/controller/sprintboard/SprintBoardController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,15 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ctrends.taskmanager.bean.WSResponse;
+import com.ctrends.taskmanager.service.sprintboard.ISprintBoardService;
 
 @RestController
 @RequestMapping("/taskman/tman/sprintboard")
 public class SprintBoardController implements ISprintBoardController {
 	
+	@Autowired
+	ISprintBoardService sprintboardService;
 
 	@RequestMapping(value = "/create", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@Override
 	public ModelAndView create(){
+		
+		
+		
 		
 		return new ModelAndView("sprintboard/sprintboard");
 		
