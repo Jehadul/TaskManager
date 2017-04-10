80c3fdf6e6b37d2b8bb5f9ee93809f90534981b3
irin <irin@gmail.com>
2017-04-10 12:38:29 +0600

    add impediment manager

---
+++ src/main/java/com/ctrends/taskmanager/controller/impedimentmanager/ImpedimentManagerController.java
@@ -0,0 +1,77 @@
+package com.ctrends.taskmanager.controller.impedimentmanager;
+
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ctrends.taskmanager.bean.WSResponse;
+
+@RestController
+@RequestMapping("/taskman/impedimentmanager")
+public class ImpedimentManagerController implements IImpedimentManagerController {
+
+	@Override
+	public ModelAndView index() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView show(UUID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WSResponse get(UUID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WSResponse store(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView edit(UUID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WSResponse update(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WSResponse destroy(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView showSearch(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String search(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
