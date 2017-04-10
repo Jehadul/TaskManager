6540a17eee8330f98b5936ef8db66759ee0c8c88
Reza <rrezaur166@gmail.com>
2017-04-10 10:48:58 +0600

    namimg conversion followed

---
+++ src/main/java/com/ctrends/taskmanager/controller/storyview/StoryViewController.java
@@ -0,0 +1,103 @@
+package com.ctrends.taskmanager.controller.storyview;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ctrends.taskmanager.bean.WSResponse;
+import com.ctrends.taskmanager.model.tman_sprint.SprintManager;
+import com.ctrends.taskmanager.service.tman_sprint.ISprintService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+@RestController
+@RequestMapping("/taskman/sprintboard")
+public class StoryViewController implements IStoryViewController {
+	
+	@Autowired
+	ISprintService sprintservice;
+
+	@RequestMapping(value = "/storyview", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@Override
+	public ModelAndView create(){
+		
+		Map<String, Object> data = new HashMap<String, Object>();
+		List<SprintManager> sprintboardlist = sprintservice.getAll();
+		
+		System.out.println(sprintboardlist.get(0).getSprintGoal()+" hello");
+		GsonBuilder gson = new GsonBuilder();
+		Gson g = gson.create();
+
+		data.put("sprintboardlist", sprintboardlist);
+		
+		return new ModelAndView("storyview/storyview", "data", data);
+		
+	}
+
+	@Override
+	public ModelAndView index() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView show(UUID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WSResponse get(UUID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+	@Override
+	public WSResponse store(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView edit(UUID id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WSResponse update(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public WSResponse destroy(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModelAndView showSearch(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String search(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
