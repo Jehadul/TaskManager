1bc71da89a3c3fc17526cabbeb834593d9d50ade
Reza <rrezaur166@gmail.com>
2017-04-10 10:48:58 +0600

    namimg conversion followed

---
+++ src/main/java/com/ctrends/taskmanager/controller/storyview/IStoryViewController.java
@@ -0,0 +1,13 @@
+package com.ctrends.taskmanager.controller.storyview;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ctrends.taskmanager.controller.ICommonController;
+import com.ctrends.taskmanager.model.tman_sprint.SprintManager;
+
+public interface IStoryViewController extends ICommonController<SprintManager>{
+
+	
+	
+
+}
