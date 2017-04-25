4ad1ae04acb3f91db333030cca92aa207ca3b34a
Farid Ahmed <w3farid@gmail.com>
2017-04-25 13:41:56 +0600

    modified

--- src/test/java/com/ctrends/taskmanager/service/report/TaskReportServiceTest.java
+++ src/test/java/com/ctrends/taskmanager/service/report/TaskReportServiceTest.java
@@ -12,6 +12,7 @@ import javax.validation.constraints.AssertTrue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -35,20 +36,19 @@ public class TaskReportServiceTest {
 	
 	
 	@Test
+	@WithMockUser("CTS0104")
 	public void testfindUserWiseDailyTasks_returndata(){
 		
 		Map<String, String> parameterMap=new HashMap<String, String>();
-		String start_date = "04-Apr-2017";
-		parameterMap.put("startdate", String.valueOf(start_date));
-		parameterMap.put("companyCode", companyCode);
-		parameterMap.put("empCode", empCode);
-		parameterMap.put("empName", empName);
-		parameterMap.put("username", username);
+		String start_date = "15-Apr-2017";
+		parameterMap.put("startdate", start_date);		
+		parameterMap.put("username", "CTS0104");
 		
 		
 		List<TaskReportView> tt= taskreportservice.findUserWiseDailyTasks(parameterMap);
 		
-		assertTrue(true);
+		//assertTrue(tt.isEmpty());
+		assertTrue(tt.size()>0);
 		
 	}
 	
