5b28d66bac3ae1f41320584602344605d45776ff
Farid Ahmed <w3farid@gmail.com>
2017-04-25 13:41:54 +0600

    modified

--- src/main/java/com/ctrends/taskmanager/controller/report/TaskReportController.java
+++ src/main/java/com/ctrends/taskmanager/controller/report/TaskReportController.java
@@ -24,6 +24,8 @@ import com.ctrends.taskmanager.service.report.ITaskReportService;
 import com.ctrends.taskmanager.service.report.TaskReportService;
 import com.ctrends.taskmanager.service.user.IUserService;
 
+
+
 @Controller
 @RequestMapping("/taskman/report")
 public class TaskReportController {
@@ -34,132 +36,49 @@ public class TaskReportController {
 	@Autowired
 	ITaskReportService taskReportService;
 	
-
+	
+	/**
+	 * 
+	 * @last edit by farid
+	 * change return beacuase not found company table and company code
+	 * this method view task log report daily hours
+	 */
 	@RequestMapping(value = "/tasklogreport", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ModelAndView tasklogreport(HttpServletRequest request) {
-		Map<String, Object> data = new HashMap<String, Object>();
-		List<Tasks> tasklogList = taskReportService.getAll();
 
-		Map<String, String> companyCodes = new HashMap<String, String>();
-		String companyCode = request.getParameter("company_code");
-
-		for (int i = 0; i < tasklogList.size(); i++) {
-			companyCodes.put(tasklogList.get(i).getCompanyCode(), tasklogList.get(i).getCompanyName());
-		}
-
-		data.put("companyCodes", companyCodes);
-		data.put("companyCode", companyCode);
-		return new ModelAndView("report/userdailyreport", "data", data);
+		return new ModelAndView("report/userdailyreport");
 	}
-	/*
-	 * @RequestMapping(value = "/generateuserwisereport", method =
-	 * RequestMethod.GET) public ModelAndView
-	 * generateUserWiseReport(ModelAndView modelAndView, HttpServletRequest
-	 * request) {
-	 * 
-	 * String companyCode = request.getParameter("company_code"); String empCode
-	 * = request.getParameter("emp_code"); String empName =
-	 * request.getParameter("emp_name"); String username =
-	 * request.getParameter("username");
-	 * 
-	 * java.util.Date startDate =
-	 * Utility.stringToDate(request.getParameter("start_date"));
-	 * 
-	 * System.out.println(companyCode + "companyCode" + empCode + "empCode" +
-	 * empName + "empName" + username + "username" + startDate + "startDate");
-	 * 
-	 * Map<String, Object> parameterDate = new HashMap<String, Object>();
-	 * Map<String, Object> parameterMap = new HashMap<String, Object>();
-	 * parameterMap.put("companyCode", companyCode); parameterMap.put("empCode",
-	 * empCode); parameterMap.put("empName", empName);
-	 * parameterMap.put("username", username); parameterDate.put("startDate",
-	 * startDate);
-	 * 
-	 * List<Tasks> tlist = taskReportService.findAllTasks(parameterMap);
-	 * List<TaskLog> tLogList = taskReportService.findAllTaskLog(parameterMap);
-	 * 
-	 * List<TaskLog> newLogList = new ArrayList<>();
-	 * 
-	 * if (tlist != null) {
-	 * 
-	 * for (int j = 0; j < tlist.size(); j++) {
-	 * 
-	 * Tasks tempTask = tlist.get(j);
-	 * 
-	 * for (int i = 0; i < tLogList.size(); i++) {
-	 * 
-	 * TaskLog tempLog = tLogList.get(i);
-	 * 
-	 * if (tempTask.getId().toString().equals(tempLog.getTaskId().toString()) &&
-	 * tempLog.getStartDate().equals(startDate)) { newLogList.add(tempLog); }
-	 * 
-	 * }
-	 * 
-	 * } }
-	 * 
-	 * List<String> timeList = new ArrayList<>(); List<String> timeRemain = new
-	 * ArrayList<>();
-	 * 
-	 * if (tlist != null) { for (int i = 0; i < tlist.size(); i++) {
-	 * timeList.add(millisToDigitalTimeFormat(tlist.get(i).getSpentTime()));
-	 * timeRemain.add(millisToDigitalTimeFormat((long)
-	 * tlist.get(i).getRemainingTime())); }
-	 * 
-	 * } if (newLogList.size() == 0) { tlist = null; timeList = null; timeRemain
-	 * = null; }
-	 * 
-	 * Map<String, Object> data = new HashMap<String, Object>();
-	 * data.put("tlist", tlist); data.put("tLogList", newLogList);
-	 * data.put("timeList", timeList); data.put("timeRemain", timeRemain);
-	 * data.put("date", request.getParameter("start_date")); data.put("empCode",
-	 * request.getParameter("emp_code")); data.put("empName",
-	 * request.getParameter("emp_name"));
-	 * 
-	 * return new ModelAndView("report/userwisedailyreport", "data", data);
-	 * 
-	 * }
-	 * 
-	 * public String millisToDigitalTimeFormat(long millis) { long second =
-	 * (millis / 1000) % 60; long minute = (millis / (1000 * 60)) % 60; long
-	 * hour = (millis / (1000 * 60 * 60)) % 24; return
-	 * String.format("%02d:%02d:%02d", hour, minute, second); }
+	
+	/**
+	 * remove compan_code and others
 	 */
-
 	@RequestMapping(value = "/generateuserwisereport", method = RequestMethod.GET)
 	public ModelAndView generateUserWiseReport(ModelAndView modelAndView, HttpServletRequest request) {
-
-		String companyCode = request.getParameter("company_code");
-		String empCode = request.getParameter("emp_code");
-		String empName = request.getParameter("emp_name");
 		String username = request.getParameter("username");
 		String startDate = request.getParameter("start_date");
+		
 
 		Map<String, String> parameterMap = new HashMap<String, String>();
-		parameterMap.put("companyCode", companyCode);
-		parameterMap.put("empCode", empCode);
-		parameterMap.put("empName", empName);
 		parameterMap.put("username", username);
 		parameterMap.put("startDate", startDate);
-
+		System.out.println(parameterMap.get("username"));
+		System.out.println(parameterMap.get("startDate"));
 		List<TaskReportView> taskViewReportList = taskReportService.findUserWiseDailyTasks(parameterMap);
 
 		Map<String, Object> data = new HashMap<String, Object>();
 
-		data.put("date", request.getParameter("start_date"));
-		data.put("empCode", request.getParameter("emp_code"));
-		data.put("empName", request.getParameter("emp_name"));
+		data.put("date", request.getParameter("start_date"));		
 		data.put("taskViewReportList", taskViewReportList);
 
 		return new ModelAndView("report/userwisedailyreport", "data", data);
 	}
-	
+
 	@RequestMapping(value = "/dailySummary", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ModelAndView dailySummary() {
 		// TODO Auto-generated method stub
 		return new ModelAndView("report/dailysummaryreport");
 	}
 
-	
 	@RequestMapping(value = "/generateDailySummaryReport/{startdate}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ModelAndView generateDailySummaryReport(@PathVariable(value = "startdate") String startdate) {
 
@@ -167,14 +86,14 @@ public class TaskReportController {
 		parameterMap.put("startDate", startdate);
 		System.out.println("Controller::::::::::::::::::::::::::::::::" + startdate);
 		Map<String, Object> log = taskReportService.getDailySummaryReportElement(parameterMap);
-		
+
 		List<DailySummary> dsum = (List<DailySummary>) log.get("dsummery");
-		for(DailySummary d:dsum){
+		for (DailySummary d : dsum) {
 			System.out.println(d.getId());
 			System.out.println(d.getSpentTimeTemp());
 		}
 
-		return new ModelAndView("report/generatedailysummaryreport","log",log);
+		return new ModelAndView("report/generatedailysummaryreport", "log", log);
 
 	}
 }
